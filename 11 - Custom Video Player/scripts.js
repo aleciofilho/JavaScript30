970bc43c7cc668645745339d3cdf6ac0f6f173e4
aleciofilho <alecio_filho@hotmail.com>
2022-04-13 17:07:53 -0300

    11/30. feat: add volume and playback speed sliders.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,6 +2,7 @@ const player = document.querySelector('.player')
 const video = player.querySelector('.viewer')
 const playButton = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip]')
+const sliders = player.querySelectorAll('.player__slider')
 
 const togglePlay = () => {
   if (video.paused) {
@@ -17,8 +18,16 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip)
 }
 
+function sliderUpdate() {
+  video[this.name] = this.value
+}
+
 video.addEventListener('click', togglePlay)
 playButton.addEventListener('click', togglePlay)
 skipButtons.forEach((button) => {
   button.addEventListener('click', skip)
+})
+sliders.forEach((slider) => {
+  slider.addEventListener('change', sliderUpdate)
+  slider.addEventListener('mousemove', sliderUpdate)
 })
