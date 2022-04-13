4b16614f325972f2c60233c1d3e391a9f689b953
aleciofilho <alecio_filho@hotmail.com>
2022-04-13 17:15:25 -0300

    11/30. feat: add progres bar auto increment with video duration.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,6 +3,7 @@ const video = player.querySelector('.viewer')
 const playButton = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip]')
 const sliders = player.querySelectorAll('.player__slider')
+const progress = player.querySelector('.progress__filled')
 
 const togglePlay = () => {
   if (video.paused) {
@@ -22,7 +23,13 @@ function sliderUpdate() {
   video[this.name] = this.value
 }
 
+function updateProgressBar() {
+  const percent = (video.currentTime / video.duration) * 100
+  progress.style.flexBasis = `${percent}%`
+}
+
 video.addEventListener('click', togglePlay)
+video.addEventListener('timeupdate', updateProgressBar)
 playButton.addEventListener('click', togglePlay)
 skipButtons.forEach((button) => {
   button.addEventListener('click', skip)
