a0120e08b846f3133b4a27e840afa7912bef5a1e
aleciofilho <alecio_filho@hotmail.com>
2022-04-13 16:51:22 -0300

    11/30. feat: add play/pause button.

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,17 @@
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const playButton = player.querySelector('.toggle')
+const skipButtons = player.querySelectorAll('[data-skip]')
+
+const togglePlay = () => {
+  if (video.paused) {
+    video.play()
+    playButton.innerText = '❚ ❚'
+  } else {
+    video.pause()
+    playButton.innerText = '►'
+  }
+}
+
+video.addEventListener('click', togglePlay)
+playButton.addEventListener('click', togglePlay)
